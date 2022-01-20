fc3e24a88d5e2d3e4b7bd6a1d78f91591d56a6a6
levaniakobidze <90929516+levaniakobidze@users.noreply.github.com>
2022-01-20 10:15:31 -0800

    sadadsad

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { Button } from "./Button"
+import { Link } from "react-router-dom"
+import "./Navbar.css"
+import DropDown from "./Dropdown"
+
+function Navbar() {
+
+    return <h1>asdasd</h1>
+
+    {/* <nav className="navbar">
+                <Link to="/" classname="navbar-logo">
+                    EPIC
+                </Link>
+                <h2>asdad</h2>
+            </nav> */}
+
+
+
+}
+
+export default Navbar;
